ec0f2e6fdc47d7a66e2a931ded03e2d494da545a
Anil Kondaveni <Anil.kondaveni@techcarrot.ae>
2025-12-30 10:48:19 +0530

    manifest changes

--- addons/azure_ad_integration/__manifest__.py
+++ addons/azure_ad_integration/__manifest__.py
@@ -3,6 +3,7 @@
     'version': '1.0.1',
     'depends': ['hr'],
     'data': [
+        'security/ir.model.access.csv',
         'views/hr_department_views.xml',
         'views/hr_employee_views.xml',
         'views/azure_license_views.xml',
